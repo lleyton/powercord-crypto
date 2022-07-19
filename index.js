b31cada22a212a62cd6fabde6b005cc0b71afddc
Lleyton Gray <lleyton@fyralabs.com>
2022-07-18 18:48:12 -0700

    fix

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const { Plugin } = require('powercord/entities');
-const { getModule, React } = require('powercord/webpack');
+const { Plugin } = require("powercord/entities");
 
-const Settings = require('./components/Settings');
-const commands = require('./commands');
+const Settings = require("./components/Settings");
+const commands = require("./commands");
 
 module.exports = class PowercordCrypto extends Plugin {
+  async startPlugin() {
+    this.loadStylesheet("style.scss");
 
-    async startPlugin() {
-        this.loadStylesheet('style.scss');
+    Object.values(commands).forEach((cmd) =>
+      powercord.api.commands.registerCommand(cmd)
+    );
 
-        Object.values(commands).forEach(cmd => powercord.api.commands.registerCommand(cmd));
-		
-		powercord.api.settings.registerSettings(this.entityID, {
-			category: this.entityID,
-			label: 'Crypto',
-			render: Settings
-		});
-		
-	}
-    pluginWillUnload() {
-        Object.values(commands).forEach(cmd => powercord.api.commands.unregisterCommand(cmd.command));
-		powercord.api.settings.unregisterSettings(this.entityID);
-	}
-}
+    powercord.api.settings.registerSettings(this.entityID, {
+      category: this.entityID,
+      label: "Crypto",
+      render: Settings,
+    });
+  }
+  pluginWillUnload() {
+    Object.values(commands).forEach((cmd) =>
+      powercord.api.commands.unregisterCommand(cmd.command)
+    );
+    powercord.api.settings.unregisterSettings(this.entityID);
+  }
+};
